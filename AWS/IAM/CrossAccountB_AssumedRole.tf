3af5c91c8a31a92be5d6ff918f3b4ac3361d0cdb
Jaret <jaret.deprin@hmhco.com>
2018-02-26 07:20:38 -0800

    Updates Role Names

--- AWS/IAM/CrossAccountB_AssumedRole.tf
+++ AWS/IAM/CrossAccountB_AssumedRole.tf
@@ -1,24 +1,26 @@
 # Trusts
 # See https://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html for possible conditions to increase security
-data "aws_iam_policy_document" "AccountB_TrustsDoc" {
+data "aws_iam_policy_document" "CrossAccountB_TrustsDoc" {
   statement {
     actions = [
       "sts:AssumeRole"
     ]
+
     principals {
-      AWS = "${aws_iam_role.AccountA_Role.arn}"
+      identifiers = ["${aws_iam_role.CrossAccountA_Role.arn}"]
+      type = "AWS"
     }
   }
 }
 
 # IAM Role
-resource "aws_iam_role" "AccountB_Role" {
+resource "aws_iam_role" "CrossAccountB_Role" {
   name               = "service_cross_account_access"
-  assume_role_policy = "${data.aws_iam_policy_document.AccountB_TrustsDoc.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.CrossAccountB_TrustsDoc.json}"
 }
 
 # Attach Policy
 resource "aws_iam_role_policy_attachment" "AccountB_Access" {
-  role      = "${aws_iam_role.AccountB_Role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+  role        = "${aws_iam_role.CrossAccountB_Role.name}"
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
